5039ddf611b70fcf47d12ea2f74465eb0d0a5984
Rob Merrill <robgmerrill@gmail.com>
2023-07-25 09:26:42 -0700

    update copy

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -9,17 +9,17 @@ const About = () => {
       <div className={styles.imgContainer}>
         <Image className={styles.img} src="https://images.pexels.com/photos/930530/pexels-photo-930530.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" fill={true} alt="about hero" />
         <div className={styles.imgText}>
-          <h1 className={styles.imgTitle}>Digital Storytellers</h1>
-          <h2 className={styles.imgDescription}>Handcrafting award winning digital expressions</h2>
+          <h1 className={styles.imgTitle}>Web Developer &amp; Instructor</h1>
+          <h2 className={styles.imgDescription}>Part Developer and Part Mentor</h2>
         </div>
       </div>
       <div className={styles.textContainer}>
         <div className={styles.item}>
-          <h1 className={styles.title}>Who Are We?</h1>
+          <h1 className={styles.title}>Who I Am</h1>
           <p className={styles.description}>loremkjsd flkjsd flkjsdf lksjdf lksjdf lksjdf lksdjf lksdj flksjd flksdj flksdj flksjd fl lskjdfl sdjf lksjd fls djflksjd flksjd flksdj flksdj flksdjf lksdj sfsd sdf sd fs df sdf s df sd fs df sd f sdf flksdj flksdj flksdjf kjd flksj dflksdj flkdsfj <br /><br /> lksdjflksd jflksdj flsdj f sdf sd fsd fs df sdf sd fssdfsdfs dfsdfsd fsdf sdf as dfasdfa sdfasdf asdf asdf asd fasdf as dfasd f df sd f sdf sdf  sdf slskdjf lsdkjf lskdjf </p>
         </div>
         <div className={styles.item}>
-        <h1 className={styles.title}>What We Do</h1>
+        <h1 className={styles.title}>What I Do</h1>
         <p className={styles.description}>loremkjsd flkjsd flkjsdf lksjdf lksjdf lksjdf lksdjf <br /><br />lksdj flksjd flksdj flksdj flksjd fl lskjdfl sdjf lksjd fls djflksjd flksjd flksdj flksdj flksdjf lksdj sfsd sdf sd fs df sdf s df sd fs df sd f sdf flksdj flksdj flksdjf kjd flksj dflksdj flkdsfj 
         </p>
         <Button url="contact" text="Contact" />
