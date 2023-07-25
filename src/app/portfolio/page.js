9d95b9f0776c840d09395df6dee31335ef306059
Rob Merrill <robgmerrill@gmail.com>
2023-07-25 10:26:41 -0700

    add porfolio layout and styling

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import styles from './page.module.css'
+import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
 
 const Portfolio = () => {
   return (
-    <div className={styles.container}>Portfolio</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Learn More</h1>
+      <div className={styles.items}>
+        <Link href="/portfolio/illustrations" className={styles.item}>
+          <span className={styles.title}>Project 1</span>
+        </Link>
+        <Link href="/portfolio/websites" className={styles.item}>
+          <span className={styles.title}>Project 2</span>
+        </Link>
+        <Link href="/portfolio/application" className={styles.item}>
+          <span className={styles.title}>Project 3</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
