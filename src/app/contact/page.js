54f498e86cbeb73e6cedf5c59e2a338a55d0430f
Rob Merrill <robgmerrill@gmail.com>
2023-08-02 10:22:32 -0700

    static and dynamic SEO

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -3,6 +3,11 @@ import styles from './page.module.css'
 import Image from 'next/image'
 import Button from '@/components/Button/Button'
 
+export const metadata = {
+  title: 'Rob Merrill Contact Information',
+  description: 'This is the contact page',
+}
+
 const Contact = () => {
   return (
     <div className={styles.container}>
